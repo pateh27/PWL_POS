bf3b82a0b2636a7b5f40b124bfc73ba370e47d73
pateh27 <fathzfl@gmail.com>
2025-03-03 22:53:29 +0700

    praktikum week 4

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index() {
+        $data = [
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ];
+        UserModel::created($data);      
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
