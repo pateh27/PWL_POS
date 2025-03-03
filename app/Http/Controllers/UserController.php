9837dcf571429f5bacfff32b05b288e7ded5b00a
pateh27 <fathzfl@gmail.com>
2025-03-04 02:01:26 +0700

    learning Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,9 +21,7 @@ class UserController extends Controller
         // $user = UserModel::firstWhere('level_id', 1);
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
