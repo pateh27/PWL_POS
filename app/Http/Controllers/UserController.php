e3225f549b2edc79da08de49bb0c92fca26caa6e
pateh27 <fathzfl@gmail.com>
2025-03-03 22:57:28 +0700

    langkah-langkah praktikum 1 week 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,8 @@ class UserController extends Controller
 {
     public function index() {
         $data = [
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
             'level_id' => 2
         ];
